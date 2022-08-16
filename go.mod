0ee5e14e2d5e40db5fee3b85d2255bb80cd4d408
feng-src <1195494025@qq.com>
2022-08-16 09:54:10 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/feng-src/go-github-action2.git
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+
+
+  for y := 1; y<=9; y++{
+	  for x  := 1; x <= y;x++{
+            fmt.Printf("%d*%d=%d ", x, y, x*y)
+	  }
+	  fmt.Println()
+
+  }
+
+
+}
